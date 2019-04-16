8b5af4e0db4ad5dc44ba9f23150c06c0780a6d56
Jack Godwin <3521886+KodingSykosis@users.noreply.github.com>
2019-04-16 08:52:33 -0400

    Update Program.cs

--- InterviewTest1/Program.cs
+++ InterviewTest1/Program.cs
@@ -9,11 +9,6 @@ namespace InterviewTest1
         {
             var data = new Repo().All();
 
-            //This example does not include all required fields for output.
-            data.SelectMany(invoice => invoice.LineItems)
-                .ToList()
-                .ForEach(Console.WriteLine);
-
             Console.ReadLine();
         }
     }
